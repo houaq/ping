7efe98607edbc81455b05e016ff0a1a58cdd1367
Atsing Hou <houaq@qq.com>
2020-09-24 16:37:18 +0800

    Remove SetControlMessage in order to run on Windows

--- ping.go
+++ ping.go
@@ -321,9 +321,9 @@ func (p *Pinger) Run() error {
 		if conn, err = p.listen(ipv4Proto[p.protocol]); err != nil {
 			return err
 		}
-		if err = conn.IPv4PacketConn().SetControlMessage(ipv4.FlagTTL, true); err != nil {
-			return err
-		}
+		// if err = conn.IPv4PacketConn().SetControlMessage(ipv4.FlagTTL, true); err != nil {
+		// 	return err
+		// }
 	} else {
 		if conn, err = p.listen(ipv6Proto[p.protocol]); err != nil {
 			return err
